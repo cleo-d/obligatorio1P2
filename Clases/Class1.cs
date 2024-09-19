88f5122535a40c47eacec9536cd3baec230dced7
DESKTOP-QUCVI71\Admin <cleo.diaz@ringcentral.com>
2024-09-19 18:35:34 -0300

    Clases creadas: -Sistema -Articulo -Publicacion -Usuario
    Menu principal generico en UI creado

--- Clases/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Clases
-{
-    public class Class1
-    {
-        //Clase 1 creada por VS por defecto
-    }
-}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,28 @@
-﻿namespace ConsoleApp1
+﻿using System.ComponentModel.Design;
+
+namespace UI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Probando pasar cambios a github
-            Console.WriteLine("Hello, World!");
+            //  3.Desplegar un menú en consola que permita:
+            //      • Listado de todos los clientes.
+            //      • Dado un nombre de categoría listar todos los artículos de esa categoría.
+            //      • Alta de artículo.
+            //      • Dadas dos fechas, listar las publicaciones entre esas fechas. Mostrar Id, nombre estado y fecha de
+            //        publicación
+
+            Console.WriteLine("Menu principal:");
+            Console.WriteLine("1-Mostrar listado de clientes");
+            Console.WriteLine("2-Listar productos segun categoria");
+            Console.WriteLine("3-Dar de alta a un articulo");
+            Console.WriteLine("4-Listar publicaciones segun rango de fechas");
+            Console.WriteLine("0-Salir");
+
+
+            Console.ReadKey();
+
         }
     }
 }
