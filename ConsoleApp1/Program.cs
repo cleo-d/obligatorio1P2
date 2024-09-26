dad699eb0d872b01b73057be2bd3be199d3b9abd
DESKTOP-QUCVI71\Admin <cleo.diaz@ringcentral.com>
2024-09-25 22:18:53 -0300

    Se le muestra al usuario un menu principal con opciones Dependiendo de la opcion ingresada el output que se vera Falta corregir ultima opcion del menu (Referencia a la funcion que toma 2 fechas y devuelve una lista de publicaciones) *Leer comentarios*

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Clases;
 using System.ComponentModel.Design;
+using System.Diagnostics;
 
 namespace UI
 {
@@ -17,60 +18,92 @@ namespace UI
 
             Console.WriteLine("Menu principal:");
             Console.WriteLine("1-Mostrar listado de clientes");
-            Console.WriteLine("-------");
-
-            List<Usuario> usuarios = s.GetUsuarios();
-            
-            foreach (Usuario u in usuarios)
-            {
-                if(u is Cliente)
-                {
-                    Cliente clienteAux = u as Cliente;
-                    Console.WriteLine(clienteAux.Nombre);
-                }
-                
-            }
-            Console.WriteLine("-------");
-
             Console.WriteLine("2-Listar productos segun categoria");
-            Console.WriteLine("-------");
-            List<Articulo> articulosPorCategoria = s.GetArticulosPorCategoria("Limpieza");
-            if (articulosPorCategoria.Count > 0)
-            {
-                foreach (Articulo Articulo in articulosPorCategoria)
-                {
-                    Console.WriteLine(Articulo.Nombre);
-                }
-            }
-            Console.WriteLine("-------");
-
             Console.WriteLine("3-Dar de alta a un articulo");
-            s.darAltaArticulo("Jabon1", "Limpieza", 80);
-
-            List<Articulo> articulosAux = s.GetArticulos();
-
-            foreach (Articulo a in articulosAux)
-            {
-            Console.WriteLine($"Nombre:  {a.Nombre }");
-
-            }
-
-            Console.WriteLine("-----------");
             Console.WriteLine("4-Listar publicaciones segun rango de fechas");
-
-            List <Publicacion> publicacionesAux = s.PublicacionesEntreFechas(new DateTime(2024, 01, 25), new DateTime(2024, 05, 25));
-
-            foreach (Publicacion p in publicacionesAux)
-            {
-                Console.WriteLine($"Id: {p.Id} Nombre: {p.Nombre} Estado: {p.Estado}");
-            }
-
-
             Console.WriteLine("0-Salir");
 
+            //Leo opcion elegida por usuario
+            string inputUsuario = Console.ReadLine();
+
+            switch (inputUsuario)
+            {
+                case "1":
+                    //Mostrar Lista de clientes
+                    List<Usuario> usuarios = s.GetUsuarios();
+            
+                    foreach (Usuario u in usuarios)
+                    {
+                        if(u is Cliente)
+                        {
+                            Cliente clienteAux = u as Cliente;
+                            Console.WriteLine(clienteAux.Nombre);
+                        }
+                    }
+                    break;
+                case "2":
+                    Console.WriteLine("Indique una categoria");
+                    string inputCategoria = Console.ReadLine();
+
+                    //Listar productos segun categoria
+                    List<Articulo> articulosPorCategoria = s.GetArticulosPorCategoria(inputCategoria);
+                    if (articulosPorCategoria.Count > 0)
+                    {
+                        foreach (Articulo Articulo in articulosPorCategoria)
+                        {
+                            Console.WriteLine(Articulo.Nombre);
+                        }
+                    }
+                    break;
+
+                case "3":
+                    //Crear Articulo nuevo
+                    Console.WriteLine("Ingrese un nombre para el Articulo");
+                    string inputNomArt = Console.ReadLine();
+                    Console.WriteLine("Ingrese una Categoria para el Articulo");
+                    string inputCatArt = Console.ReadLine();
+                    Console.WriteLine("Ingrese un Precio para el Articulo");
+                    double inputPrecioArt = int.Parse(Console.ReadLine());
+                    s.darAltaArticulo(inputNomArt,inputCatArt,inputPrecioArt);
+
+                    Console.WriteLine($"Articulo ingresado: {inputNomArt}, Categoria: {inputCatArt}, Precio: {inputPrecioArt}");
+
+
+                    break;
+
+                case "4":
+                    //Listar publicaciones segun rango de fechas
+                    //Todavia hay que terminar esto, hay que leer bien el input de la fecha y pasarlo a una instancia de DateTime
+                    Console.WriteLine("Ingrese una fecha de inicio con formato AAAA-MM-DD");
+                    DateTime inputFecha1 = DateTime.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese una fecha de finalizacion con formato AAAA-MM-DD");
+                    DateTime inputFecha2 = DateTime.Parse(Console.ReadLine()); ;
+
+                    Console.WriteLine(inputFecha1.ToString(),  inputFecha2.ToString());
+
+                    List<Publicacion> publicacionesAux = s.PublicacionesEntreFechas(inputFecha1,inputFecha2);
+                    foreach (Publicacion p in publicacionesAux)
+                    {
+                        Console.WriteLine($"Id: {p.Id} Nombre: {p.Nombre} Estado: {p.Estado}");
+                    }
+
+                    break;
+
+                case "0":
+                    Console.WriteLine("Saliendo del sistema");
+                    break;
+
+                default:
+                    Console.WriteLine("Por favor ingrese una opcion del menu");
+                    break;
+            }
 
             Console.ReadKey();
 
+
+
+
+            //End
         }
     }
 }
